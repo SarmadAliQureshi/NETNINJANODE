ff3e0f98888bd291ab5040239ef4e74000997c9d
SarmadAliQureshi <sarmadalikaharian@gmail.com>
2020-02-12 21:14:16 +0500

    query string

--- app.js
+++ app.js
@@ -136,7 +136,8 @@ server.listen(8080,'127.0.1.1')
  })
 
  app.get('/contact',function(req,res){
-     res.render('contact')
+     console.log(req.query)
+     res.render('contact',{qs:req.query})
  })
 
  app.get('/profile/:name',function(req,res){
